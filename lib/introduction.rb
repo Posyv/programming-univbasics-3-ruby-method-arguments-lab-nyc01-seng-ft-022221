a875e27be1a0dede2eace2383dd553032d1cd415
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-11 22:09:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,15 +4,11 @@ end
 introduction("Edwin")
 
 def introduction_with_language (name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am going to learn to program in #{language}."
 end
-introduction_with_language("Edwin", "Python")
+introduction_with_language("Charlie", "Ruby")
 introduction_with_language("Dan", "Ember.js")
 
-def introduction_with_language_optional (name, language = "Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-introduction_with_language_optional("Edwin", "Python")
 
 
 
